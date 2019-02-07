1c0095d511840c874439f9d10456ee22ecd295db
jayapranata <jaya_pranata@hotmail.co.id>
2019-02-07 13:12:15 +0700

    WIP 1

---
+++ Groowy/Controller/EyeSKScene.swift
@@ -0,0 +1,13 @@
+//
+//  EyeSKScene.swift
+//  Groowy
+//
+//  Created by Jaya Pranata on 2/7/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class EyeSKScene: SKScene {
+
+}
