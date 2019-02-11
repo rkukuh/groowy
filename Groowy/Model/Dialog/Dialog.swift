dec33cca4b0e1771c7176ff398605082ca4adf58
R. Kukuh <rkukuh@gmail.com>
2019-02-11 16:26:56 +0700

    Add model for managing a dialogs

---
+++ Groowy/Model/Dialog/Dialog.swift
@@ -0,0 +1,9 @@
+//
+//  Dialog.swift
+//  Groowy
+//
+//  Created by R. Kukuh on 11/02/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import Foundation
---
+++ Groowy/Model/Dialog/DialogRule.swift
@@ -0,0 +1,14 @@
+//
+//  Rules.swift
+//  Groowy
+//
+//  Created by R. Kukuh on 11/02/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import Foundation
+
+struct Rules {
+    var category: String?
+    var dialogs: [Dialog] = []
+}
