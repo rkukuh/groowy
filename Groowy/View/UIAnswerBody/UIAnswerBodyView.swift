34d180d5561a573665e9941e6de3461e3ad5edc8
jayapranata <jaya_pranata@hotmail.co.id>
2019-02-11 09:09:18 +0700

    Answer Body

---
+++ Groowy/View/UIAnswerBody/UIAnswerBodyView.swift
@@ -0,0 +1,21 @@
+//
+//  UIAnswerBodyView.swift
+//  Groowy
+//
+//  Created by Jaya Pranata on 2/11/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class UIAnswerBodyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
