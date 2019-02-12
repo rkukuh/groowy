7ede027bcae01c74999bbfa09c52aba6ed27beea
R. Kukuh <rkukuh@gmail.com>
2019-02-12 16:40:36 +0700

    Add data model

---
+++ Groowy/Model/Challenge/ChallengeStatus.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Groowy
+//
+//  Created by R. Kukuh on 12/02/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import Foundation
+
+enum ChallengeStatus: String {
+    case Canceled = "Canceled"
+    case Completed = "Completed"
+    case Progress = "Progress"
+}
