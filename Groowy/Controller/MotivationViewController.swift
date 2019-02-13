2e966ef0d8ab51e10e2274379b485f6e87a6a420
R. Kukuh <rkukuh@gmail.com>
2019-02-13 12:02:16 +0700

    Create blank view controller

---
+++ Groowy/Controller/MotivationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MotivationViewController.swift
+//  Groowy
+//
+//  Created by R. Kukuh on 13/02/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class MotivationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
