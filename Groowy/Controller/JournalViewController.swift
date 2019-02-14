f818d5d27d9118cde0e545c088f2422679e838c1
jayapranata <jaya_pranata@hotmail.co.id>
2019-02-14 13:52:14 +0700

    Checkpoint 1

---
+++ Groowy/Controller/JournalViewController.swift
@@ -0,0 +1,46 @@
+//
+//  JournalViewController.swift
+//  Groowy
+//
+//  Created by Jaya Pranata on 2/14/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class JournalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+
+    @IBOutlet weak var spriteKit: SKView!
+    @IBOutlet weak var journalTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
