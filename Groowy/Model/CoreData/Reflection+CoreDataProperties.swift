4105a9a80ac672cade1cdee9ad6a8caa62a6ade6
R. Kukuh <rkukuh@gmail.com>
2019-02-15 11:46:13 +0700

    Edit Reflection entity's columns

--- Groowy/Model/CoreData/Reflection+CoreDataProperties.swift
+++ Groowy/Model/CoreData/Reflection+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Reflection+CoreDataProperties.swift
 //  Groowy
 //
-//  Created by R. Kukuh on 13/02/19.
+//  Created by R. Kukuh on 15/02/19.
 //  Copyright © 2019 R. Kukuh. All rights reserved.
 //
 //
@@ -17,9 +17,11 @@ extension Reflection {
         return NSFetchRequest<Reflection>(entityName: "Reflection")
     }
 
-    @NSManaged public var body: String?
+    @NSManaged public var feel: String
     @NSManaged public var created_at: NSDate?
     @NSManaged public var picture: NSData?
+    @NSManaged public var learn: String
+    @NSManaged public var struggle: String
     @NSManaged public var challenge: Challenge?
 
 }
