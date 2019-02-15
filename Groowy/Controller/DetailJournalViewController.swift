42d86daf9a1c8412043bfe56709aa07fd59a093f
jayapranata <jaya_pranata@hotmail.co.id>
2019-02-15 16:30:19 +0700

    Detail Journal part 1

--- Groowy/Controller/DetailJournalViewController.swift
+++ Groowy/Controller/DetailJournalViewController.swift
@@ -13,11 +13,15 @@ class DetailJournalViewController: UIViewController {
     @IBOutlet weak var spriteKitView: SKView!
     var scene: GameScene!
     var timer: Timer!
+    var challange:Challenge?
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGameScene()
         awake()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func closeDetailJournal(_ sender: UIButton) {
+        dismiss(animated: false, completion: nil)
     }
     
     func setupGameScene() {
@@ -45,14 +49,17 @@ class DetailJournalViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if (segue.identifier == "embedDetailJournal"){
+            if let detail = segue.destination as? DetailJournalTableViewController{
+                detail.chalange = self.challange
+            }
+        }
     }
-    */
+ 
 
 }
